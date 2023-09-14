d1d7b7d6036a03f21a95814defd4b3e0fdc7a21e
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-14 20:26:50 +0200

    variable+syntaxis+operaciones

--- saludos.py
+++ saludos.py
@@ -1,1 +1,19 @@
 print("Hello World!")
+
+name = "Irina"
+print("Hello " + name + "!")
+
+name = "Karina"
+print("Hello " + name + "!")
+
+resultado = (((1 + 2) * 3) / 4)
+print(resultado)
+
+resultado = (-1/4)
+print(resultado)
+
+resultado = (1/3)
+print(resultado)
+
+resultado = (2**10)
+print(resultado)
