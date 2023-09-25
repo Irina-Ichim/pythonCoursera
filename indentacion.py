68f4358f8307137e055a7ce9fe9ad7a0b6ea99a9
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-25 20:56:03 +0200

    Indentación

---
+++ indentacion.py
@@ -0,0 +1,27 @@
+"""
+Este es un ejemplo de docstring que explica la estructura de control 'if' en Python.
+
+El 'if' se utiliza para realizar una evaluación condicional en el código. Si la condición especificada en el 'if' es verdadera (True), se ejecutan las instrucciones dentro del bloque 'if'. Si la condición es falsa (False), se omite el bloque 'if' y se ejecutan las instrucciones después del 'if'.
+
+Ejemplo:
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
+
+En este ejemplo, la condición 'edad >= 18' se evalúa. Si es verdadera, se imprimirá "Eres mayor de edad.". Si es falsa, se imprimirá "Eres menor de edad.".
+
+La indentación es esencial en Python para definir el alcance de las estructuras de control. Todas las líneas indentadas están dentro del bloque condicional 'if' o 'else'.
+
+Recuerda que los docstrings son cadenas de texto utilizadas para documentar el código y proporcionar información sobre su funcionamiento.
+
+"""
+
+# Aquí comienza el código real
+edad = 20
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
