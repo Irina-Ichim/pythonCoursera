4e02824ea0b776164e63e0664e0ccb1dc49c7c79
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-26 13:03:10 +0200

    else_if

---
+++ elseif.py
@@ -0,0 +1,14 @@
+"""
+    Verifica si un número es par o impar.
+
+    Parameters:
+    numero (int): El número que se desea verificar.
+
+    Returns:
+    bool: True si el número es par, False si es impar.
+    """
+def verificar_paridad(numero):
+    if numero % 2 == 0:
+        return True
+    return False
+
