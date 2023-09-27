2e2a1f7d82291356397fe507ba4abe5854fe3e4a
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-27 18:58:08 +0200

    nestedloop o bucles anidados

---
+++ nestedloops.py
@@ -0,0 +1,27 @@
+# Nested loop =  bucle anidado
+
+#for left in range(7):
+   # for right in range (left , 7):
+      #  print("[" + str(left) + "|" + str(right) + "]", end = " ")
+    #print()
+    
+    
+#equipos = ['Barcelona', 'Madrid', 'Valencia', 'Malaga']  # equipos
+#for home_equipo in equipos:
+   # for visitantes_equipo in equipos:
+    #    if home_equipo != visitantes_equipo:
+    #        print(home_equipo + " vs " + visitantes_equipo)
+
+#amigas = ['Montse', 'Ikram', 'Minerva']
+#amiga_deseada = 'Montse'
+#for amiga in amigas:
+ #   if amiga_deseada == amiga:
+ #       print(amiga)
+  
+  # Necesito conseguir de resultado 12, 18, 24, 30, 36  
+  
+for n in range(6, 18 +1, 3):
+  print(n*2)  
+ # print(n+2)
+   
+  
