4f730a9a983e8bb3e37c0923757a2f0ca2c2e2d7
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-30 21:29:06 +0200

    ejemplos de metodos de listas

---
+++ practicando_listas.py
@@ -0,0 +1,20 @@
+# Crear una lista
+my_list = ["apple", "banana", "cherry"]
+
+# Agregar un elemento al final de la lista
+my_list.append("orange")
+
+# Agregar un elemento en una posición específica (al frente en este caso)
+my_list.insert(0, "grape")
+
+# Eliminar un elemento específico
+my_list.remove("banana")
+
+# Eliminar y obtener un elemento en una posición específica
+popped_element = my_list.pop(1)
+
+# Modificar un elemento mediante indexación directa
+my_list[0] = "kiwi"
+
+# Imprimir la lista actualizada
+print(my_list)
