e4ebe0bda1f30bb5f3e67559e515ed871faf3a4f
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-01 13:49:20 +0200

    repasando listas y listas comprimidas

---
+++ revision_string_listas.py
@@ -0,0 +1,46 @@
+# study_guide.py
+
+def common_sequence_operations():
+    """
+    Common sequence operations for lists and tuples.
+    """
+    pass
+
+def list_specific_operations_and_methods():
+    """
+    List-specific operations and methods.
+    """
+    pass
+
+def list_comprehensions():
+    """
+    List comprehensions in Python.
+    """
+    pass
+
+def coding_skills():
+    """
+    Coding skills related to lists and tuples.
+    """
+    pass
+
+def main():
+    """
+    Main function to display study guide content.
+    """
+    print("Lists and Tuples Study Guide\n")
+
+    print("Common Sequence Operations:")
+    common_sequence_operations()
+
+    print("\nList-Specific Operations and Methods:")
+    list_specific_operations_and_methods()
+
+    print("\nList Comprehensions:")
+    list_comprehensions()
+
+    print("\nCoding Skills:")
+    coding_skills()
+
+if __name__ == "__main__":
+    main()
