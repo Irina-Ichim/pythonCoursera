2cb94ba30288562b2287cf5ce5379f0f2c2e3e49
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-30 14:57:17 +0100

    propando un edge case en testing y explicación

--- calculator_test.py
+++ calculator_test.py
@@ -8,6 +8,21 @@ class TestCalculator(unittest.TestCase):   # Define una nueva clase llamada Test
         self.assertEqual(add(1, 2), 3)   #  Esta línea es una aserción que verifica si la llamada a add(1, 2) es igual a 3. Si la función add no devuelve 3, la prueba fallará.
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        
+    
+    def test_add_large_numbers(self):
+        result = add(10**1000, 10**1000)
+        self.assertEqual(result, 2 * (10**1000))  # La suma de dos números muy grandes debería ser el doble del número grande.
 
 if __name__ == '__main__':     # Comprueba si el script se está ejecutando como el programa principal. Esto es necesario para garantizar que las pruebas solo se ejecuten cuando ejecutes este archivo directamente.
     unittest.main()            #  Si el script se está ejecutando como el programa principal, esta línea inicia la ejecución de las pruebas unitarias definidas en la clase TestCalculator.
+
+#  Los "edge cases" son situaciones o condiciones extremas que están fuera de los escenarios normales que tu código debe ser capaz de manejar. 
+#  Estos casos son importantes de probar porque a menudo revelan problemas o errores en tu código que podrían no ser evidentes 
+#  en condiciones normales.
+# Casos :
+# Números muy grandes o muy pequeños.
+# Cadenas vacías o de longitud máxima.
+# Valores nulos o valores extremos.
+# Situaciones en las que un cálculo podría causar un desbordamiento de memoria o errores matemáticos.
+# Escenarios en los que el código debe lidiar con limitaciones de recursos, como memoria o velocidad de procesamiento.
