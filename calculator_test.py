74ddf4c4cd71e07d2ed2ae68909249397c60b734
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-30 18:37:49 +0100

    agregando un test adicional y mas explicacion

--- calculator_test.py
+++ calculator_test.py
@@ -13,6 +13,13 @@ class TestCalculator(unittest.TestCase):   # Define una nueva clase llamada Test
     def test_add_large_numbers(self):
         result = add(10**1000, 10**1000)
         self.assertEqual(result, 2 * (10**1000))  # La suma de dos números muy grandes debería ser el doble del número grande.
+        
+     # aditional test   
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-5, -3), -8)
+        self.assertEqual(add(-10, 5), -5)
+        self.assertEqual(add(0, -7), -7)
+
 
 if __name__ == '__main__':     # Comprueba si el script se está ejecutando como el programa principal. Esto es necesario para garantizar que las pruebas solo se ejecuten cuando ejecutes este archivo directamente.
     unittest.main()            #  Si el script se está ejecutando como el programa principal, esta línea inicia la ejecución de las pruebas unitarias definidas en la clase TestCalculator.
@@ -26,3 +33,7 @@ if __name__ == '__main__':     # Comprueba si el script se está ejecutando como
 # Valores nulos o valores extremos.
 # Situaciones en las que un cálculo podría causar un desbordamiento de memoria o errores matemáticos.
 # Escenarios en los que el código debe lidiar con limitaciones de recursos, como memoria o velocidad de procesamiento.
+#Los tests unitarios se enfocan en la funcionalidad principal de una unidad de código,
+# los tests de casos límite evalúan situaciones excepcionales o extremas,
+# y los tests adicionales amplían la cobertura de pruebas considerando diversos escenarios. 
+# Es común que los tests de casos límite se incluyan como parte de los tests unitarios o de los tests adicionales.
