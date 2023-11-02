a48a5ab6c1b87260d2b28ecfa711f504582c61ea
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-11-02 19:58:31 +0100

    comandos adicionales bash

---
+++ comandos_adicionales_bash.py
@@ -0,0 +1,61 @@
+# Comandos Útiles de la Línea de Comandos en Unix y Linux
+
+# Comando: grep
+# Propósito: Busca patrones de texto en archivos o flujos de datos.
+# Ejemplo: grep "patrón" archivo.txt
+
+# Comando: find
+# Propósito: Busca archivos y directorios en el sistema de archivos.
+# Ejemplo: find /ruta -name "archivo.txt"
+
+# Comando: tar
+# Propósito: Permite comprimir y descomprimir archivos y directorios.
+# Ejemplo: tar -czvf archivo.tar.gz directorio/
+
+# Comando: ssh
+# Propósito: Permite conectarse de forma segura a máquinas remotas.
+# Ejemplo: ssh usuario@servidor
+
+# Comando: wget y curl
+# Propósito: Descargan archivos desde la web en la línea de comandos.
+# Ejemplo: wget URL_del_archivo
+# Ejemplo: curl -O URL_del_archivo
+
+# Comando: top
+# Propósito: Muestra información en tiempo real sobre el uso de recursos del sistema.
+# Ejemplo: top
+
+# Comando: ps
+# Propósito: Muestra información sobre los procesos en ejecución.
+# Ejemplo: ps aux
+
+# Comando: kill
+# Propósito: Termina un proceso en ejecución.
+# Ejemplo: kill ID_de_proceso
+
+# Comando: sed
+# Propósito: Permite realizar manipulación de texto y sustitución en archivos.
+# Ejemplo: sed 's/patrón/reemplazo/' archivo.txt
+
+# Comando: awk
+# Propósito: Procesa y analiza datos de texto de forma eficiente.
+# Ejemplo: awk '{print $2}' archivo.txt
+
+# Comando: ping
+# Propósito: Comprueba la conectividad de red con una dirección IP o un nombre de dominio.
+# Ejemplo: ping google.com
+
+# Comando: ifconfig
+# Propósito: Muestra y configura la información de red en sistemas Unix-like.
+# Ejemplo: ifconfig
+
+# Comando: df y du
+# Propósito: Muestran información sobre el espacio en disco y su uso.
+# Ejemplo: df -h
+# Ejemplo: du -sh directorio/
+
+# Comando: alias
+# Propósito: Crea alias o abreviaturas para comandos largos o frecuentemente utilizados.
+# Ejemplo: alias ll='ls -al'
+
+# ¡Estos son algunos de los comandos útiles en la línea de comandos! Puedes explorar más opciones y argumentos para cada comando utilizando las páginas de manual (man).
